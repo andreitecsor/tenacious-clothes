759b6e08bcfc9da6812900784e5665318604b3c9
andreitecsor <andrei.tecsor@gmail.com>
2021-04-02 15:10:10 +0300

    Sign in component created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInUpPage from "./pages/sign-in-up/sing-in-up.components";
 import Header from './components/header/header.component';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInUpPage}/>
             </Switch>
         </div>
     );
---
+++ src/pages/sign-in-up/sing-in-up.components.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component'
+import './sing-in-up.styles.scss';
+
+const SignInUpPage = () => (
+    <div className='sign-in-up'>
+        <SignIn/>
+    </div>
+)
+
+export default SignInUpPage;
