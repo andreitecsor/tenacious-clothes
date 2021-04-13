b8a93463076447337f05dc1d1254856ca4df9e07
andreitecsor <andrei.tecsor@gmail.com>
2021-04-14 00:07:43 +0300

    refactor typos

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Directory from "../../components/directory/directory.component";
 
-import './homepage.style.scss';
+import './homepage.styles.scss';
 
 
 const HomePage = () => (
