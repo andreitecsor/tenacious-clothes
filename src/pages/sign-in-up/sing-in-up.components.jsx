1ea24a570c9ae763d856c91dd9cd6ef011525fa1
andreitecsor <andrei.tecsor@gmail.com>
2021-04-19 15:58:05 +0300

    sign-up with email and save to firestore

--- src/pages/sign-in-up/sing-in-up.components.jsx
+++ src/pages/sign-in-up/sing-in-up.components.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from "../../components/sign-up/sign-up.component";
 import './sing-in-up.styles.scss';
 
 const SignInUpPage = () => (
     <div className='sign-in-up'>
         <SignIn/>
+        <SignUp/>
     </div>
 )
 
