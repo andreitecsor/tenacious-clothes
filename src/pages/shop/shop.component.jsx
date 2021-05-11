d4cb3c54fefe0ee0c41649497e70aec5c8f35f6a
andreitecsor <andrei.tecsor@gmail.com>
2021-05-11 19:36:24 +0300

    respective selectors, updated components with new redux flow for directory and shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import {Route} from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
-import {selectCollections} from "../../redux/shop/shop.selectors";
-import {connect} from "react-redux";
-import {createStructuredSelector} from "reselect";
-
-const ShopPage = ({collections}) => (
+const ShopPage = ({match}) => (
     <div className='shop-page'>
-        {
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
-            ))
-        }
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path ={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-        collections: selectCollections
-    }
-);
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,5 +1,5 @@
-const SHOP_DATA = [
-  {
+const SHOP_DATA = {
+  hats: {
     id: 1,
     title: 'Hats',
     routeName: 'hats',
@@ -60,7 +60,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  sneakers:{
     id: 2,
     title: 'Sneakers',
     routeName: 'sneakers',
@@ -115,7 +115,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  jackets:{
     id: 3,
     title: 'Jackets',
     routeName: 'jackets',
@@ -152,9 +152,9 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  womens:{
     id: 4,
-    title: 'Womens',
+    title: "Women's",
     routeName: 'womens',
     items: [
       {
@@ -201,9 +201,9 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  mens:{
     id: 5,
-    title: 'Mens',
+    title: "Men's",
     routeName: 'mens',
     items: [
       {
@@ -244,6 +244,6 @@ const SHOP_DATA = [
       }
     ]
   }
-];
+};
 
 export default SHOP_DATA;
